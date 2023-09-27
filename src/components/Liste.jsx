f82e713ddd3a1893e9b2e5071623cf22a45802e0
lamnaourm <mohammed.lamnaour@gmail.com>
2023-09-27 16:30:04 +0100

    condition

--- src/components/Liste.jsx
+++ src/components/Liste.jsx
@@ -2,7 +2,7 @@
 function Liste({elements}) {
     return <ul>
         {elements.map(e => 
-            <li key={e.id}>{e.id} - {e.text}</li>
+            <li key={e.id} style={{color:e.completed && 'green' }}>{e.id} - {e.text}</li>
             )}
     </ul>;
 }
